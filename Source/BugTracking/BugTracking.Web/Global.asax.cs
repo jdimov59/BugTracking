57815d1e4b563e5e903fadc6fa7a51c083da0d47
jdimov59 <jdimov59@gmail.com>
2016-05-26 15:06:11 +0300

    adding migration

--- Source/BugTracking/BugTracking.Web/Global.asax.cs
+++ Source/BugTracking/BugTracking.Web/Global.asax.cs
@@ -16,6 +16,7 @@ namespace BugTracking.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<DataContext>());
         }
     }
 }
